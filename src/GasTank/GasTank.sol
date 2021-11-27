b448dc464f8dd22e3c9d61b30396bf314e0e4fe2
Ashley Barr <ashley.barr@ar4s.com>
2021-11-27 00:38:53 -0600

    add function & tests

--- src/GasTank/GasTank.sol
+++ src/GasTank/GasTank.sol
@@ -4,34 +4,70 @@ pragma solidity 0.8.9;
 import "hardhat-deploy/solc_0.8/proxy/Proxied.sol";
 import "hardhat/console.sol";
 
+contract Depositor {
+
+    receive() external payable {}
+}
+
+
+
 contract GasTank is Proxied {
     string internal _prefix;
     string public test;
-    // event FallbackLog(uint256 gas);
+    address public target = msg.sender;
     event DepositLog(uint256 gas);
     event FallbackLog(string text);
     event TestChanged(string _test);
 
-    // Fallback function must be declared as external.
-    fallback() external payable {
-        // send / transfer (forwards 2300 gas to this fallback function)
-        // call (forwards all of the gas)
-        emit FallbackLog("hi");
-        console.log("fallback");
+        constructor(string memory prefix) {
+        postUpgrade(prefix);
     }
+
+    Depositor private receiverAdr = new Depositor();
+
+    function sendEther(uint _amount) public payable {
+        // if (!address(receiverAdr).send(_amount)) {
+        //     //handle failed send
+        // }
+    }
+
+    function callValueEther(uint _amount) public payable {
+        (bool success,) = address(receiverAdr).call{value: _amount, gas: 35000}("");
+        require(success);
+    }
+
+    function transferEther(uint _amount) public payable {
+        payable(receiverAdr).transfer(_amount);
+    }
+
+    // Fallback function must be declared as external.
+    // fallback() external payable {
+    //     // send / transfer (forwards 2300 gas to this fallback function)
+    //     // call (forwards all of the gas)
+    //     payable(target).transfer(msg.value/50);
+    //     emit FallbackLog("hi");
+    //     console.log("fallback");
+    // }
 
     receive() external payable {
         // send / transfer (forwards 2300 gas to this fallback function)
         // call (forwards all of the gas)
+        if (address(this).balance > 10 ether) {
+            revert("GasTank is already full");
+            console.log("revert");
+        }
+        payable(target).transfer(msg.value/2);
+        // target.transfer(msg.value/100);
         emit DepositLog(gasleft());
         console.log("receive");
     }
+
 
     // Helper function to check the balance of this contract
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
-
+    // console.log("target");
     function postUpgrade(string memory prefix) public proxied {
         _prefix = prefix;
     }
@@ -39,9 +75,5 @@ contract GasTank is Proxied {
     function setTest(string memory _test) public {
         test = string(abi.encodePacked(_prefix, _test));
         emit TestChanged(_test);
-    }
-
-    constructor(string memory prefix) {
-        postUpgrade(prefix);
     }
 }
--- test/GasTank.test.ts
+++ test/GasTank.test.ts
@@ -5,6 +5,7 @@ import {swapExactETHForTokens} from './helpers/uniswap-functions';
 import {getMainnetSdk} from '@dethcrypto/eth-sdk-client';
 import {GasTank} from '../typechain';
 import { UniswapV2Router02, USDC } from '@dethcrypto/eth-sdk-client/types';
+import { BN , expectRevert} from '@openzeppelin/test-helpers';
 
 const setup = deployments.createFixture(async () => {
   await deployments.fixture('GasTank');
@@ -34,11 +35,17 @@ const setup = deployments.createFixture(async () => {
   };
 });
 
+
 describe('GasTank', function () {
   describe('Initialization', function () {
     let _GasTank: GasTank;
     let _UniswapV2Router02: UniswapV2Router02;
     let _USDC: USDC;
+
+    beforeEach(async function () {
+
+    });
+
 
     it('superUser initial USDC balance of 0', async () => {
       const {superUser, GasTank, UniswapV2Router02, USDC } = await setup();
@@ -48,22 +55,28 @@ describe('GasTank', function () {
 
       const usdcBalanceWei = await superUser.USDC.balanceOf(superUser.address);
       const usdcBalance = ethers.utils.formatUnits(usdcBalanceWei, 18);
+
       expect(parseFloat(usdcBalance)).to.equal(0);
     });
     it('superUser initial balance of 1000 ETH)', async () => {
       const {superUser} = await setup();
+
       const ethBalanceWei = await ethers.provider.getBalance(superUser.address);
       const ethBalance = ethers.utils.formatEther(ethBalanceWei);
+
       expect(parseFloat(ethBalance)).to.equal(10000);
     });
     it('Swap ETH for USDC', async () =>  {
       const {superUser, USDC} = await setup();
+
       await swapExactETHForTokens(_UniswapV2Router02, 1, USDC, superUser.address);
+
       const usdcBalanceWei = await USDC.balanceOf(superUser.address);
       const usdcBalance = ethers.utils.formatUnits(usdcBalanceWei, 6);
       expect(parseFloat(usdcBalance)).to.be.greaterThan(0);
     });
     it('superUser can send ETH to EOA', async () => {
+
       const {unnamedUsers, superUser} = await setup();
       const sender = superUser;
       const receiver = unnamedUsers[1];
@@ -94,22 +107,31 @@ describe('GasTank', function () {
     //@ts-ignore
     let _superUser;
 
+    beforeEach(async function () {
+      this.value = new BN(10000);
+    });
+
     it('event is logged', async function () {
+
       const {superUser, GasTank, UniswapV2Router02, USDC } = await setup();
+
         _GasTank = GasTank;
         _UniswapV2Router02 = UniswapV2Router02;
         _USDC = USDC;
         //@ts-ignore
-        _superUser //@ts-ignore
-      // eslint-disable-line @typescript-eslint/no-unused-vars
+        _superUser
+
       const testString = 'Peaches';
-      await expect(superUser.GasTank.setTest(testString)).to.emit(_GasTank, 'TestChanged').withArgs(testString);
+
+      await expect(superUser.GasTank.setTest(testString)).to.emit(GasTank, 'TestChanged').withArgs(testString);
     });
     it('superUser can send ETH to GasTank', async () => {
-      const {superUser} = await setup();
-      // const wallet =  ethers.Wallet.createRandom().connect(superUser.provider);
+      const {superUser, GasTank} = await setup();
+      const balance = await GasTank.getBalance()
+      //@ts-ignore
+      console.log(`balance`, parseFloat(balance))
       const sender = superUser;
-      const receiver = _GasTank;
+      const receiver = GasTank;
 
       // Create Tx Object
       const tx = {
@@ -119,24 +141,51 @@ describe('GasTank', function () {
 
       // Sign and Send Tx - Wait for Receipt
       //@ts-ignore
-      const createReceipt = await superUser.signer.sendTransaction(tx);
+      const createReceipt = await sender.signer.sendTransaction(tx);
       await createReceipt.wait();
 
       const senderEthBalance = ethers.utils.formatEther(await ethers.provider.getBalance(sender.address));
       const receiverEthBalance = ethers.utils.formatEther(await ethers.provider.getBalance(receiver.address));
+
       expect(parseFloat(senderEthBalance)).to.be.lessThan(10000);
       expect(parseFloat(receiverEthBalance)).to.be.greaterThan(0);
     });
+    it('ETH sent to GasTank is partially refunded to sender', async () => {
+      const {superUser, GasTank} = await setup();
+
+      const sender = superUser;
+      const receiver = GasTank;
+
+      // Create Tx Object
+      const tx = {
+        to: receiver.address,
+        value: ethers.utils.parseEther('1'),
+      };
+
+
+      // Sign and Send Tx - Wait for Receipt
+      //@ts-ignore
+      const createReceipt = await sender.signer.sendTransaction(tx);
+      await createReceipt.wait();
+
+      const senderEthBalance = ethers.utils.formatEther(await ethers.provider.getBalance(sender.address));
+      const receiverEthBalance = ethers.utils.formatEther(await ethers.provider.getBalance(receiver.address));
+
+      expect(parseFloat(Number(receiverEthBalance).toFixed())).to.be.within(0.2, 1);
+      expect(parseFloat(Number(senderEthBalance).toFixed())).to.be.within(9995, 10000);
+    });
     it('Calling GasTank getBalance() returns correct balance', async () => {
-      const gasTankBalance = ethers.utils.formatEther(await ethers.provider.getBalance(_GasTank.address));
-      const GasTank_getBalance = ethers.utils.formatEther(await _GasTank.getBalance());
+      const {superUser, GasTank} = await setup();
+      const gasTankBalance = ethers.utils.formatEther(await ethers.provider.getBalance(GasTank.address));
+      const GasTank_getBalance = ethers.utils.formatEther(await GasTank.getBalance());
+
       expect(parseFloat(GasTank_getBalance)).to.be.equals(parseFloat(gasTankBalance));
     });
     it('emit works', async function () {
-      const {superUser} = await setup();
+      const {superUser, GasTank} = await setup();
 
       const sender = superUser;
-      const receiver = _GasTank;
+      const receiver = GasTank;
 
       //Create Tx Object
       const tx = {
@@ -149,7 +198,7 @@ describe('GasTank', function () {
       const createReceipt = await sender.signer.sendTransaction(tx);
       await createReceipt.wait();
 
-      await expect(_GasTank.fallback()).to.emit(_GasTank, 'DepositLog');
+      await expect(GasTank.fallback()).to.emit(GasTank, 'DepositLog');
     });
   });
 });
